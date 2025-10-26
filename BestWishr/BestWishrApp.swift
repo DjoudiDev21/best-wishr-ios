544d69609dd9714ee3f799328fba752d0ece23b2
Djoudi21 <abdelkrim.djoudi@gmail.com>
2025-10-26 02:09:28 +0100

    Initial Commit

---
+++ BestWishr/BestWishrApp.swift
@@ -0,0 +1,17 @@
+//
+//  BestWishrApp.swift
+//  BestWishr
+//
+//  Created by Abdelkrim Djoudi on 26/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct BestWishrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BestWishr/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BestWishr
+//
+//  Created by Abdelkrim Djoudi on 26/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
